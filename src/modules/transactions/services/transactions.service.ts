671f3b75635c52376813d749deb84f41242d12f1
giacomo <giacomocamposv2@hotmail.com>
2025-03-14 22:19:10 -0300

    Add type and bank account filter

--- src/modules/transactions/services/transactions.service.ts
+++ src/modules/transactions/services/transactions.service.ts
@@ -5,6 +5,7 @@ import { TransactionsRepository } from 'src/database/repositories/transactions.r
 import { ValidateBankAccountOwnershipService } from '../../bank-accounts/services/validate-bank-account-ownership.service'
 import { ValidateCategoryOwnershipService } from '../../categories/services/validate-category-ownership.service'
 import { ValidateTransactionOwnershipService } from './validate-transaction-ownership.service'
+import { TransactionType } from '../entities/Transaction'
 
 @Injectable()
 export class TransactionsService {
@@ -26,8 +27,26 @@ export class TransactionsService {
     })
   }
 
-  findAllByUserId(userId: string) {
-    return this.transactionRepo.findMany({ where: { userId } })
+  findAllByUserId(
+    userId: string,
+    filters: {
+      month: number
+      year: number
+      bankAccountId?: string
+      type?: TransactionType
+    }
+  ) {
+    return this.transactionRepo.findMany({
+      where: {
+        userId,
+        bankAccountId: filters.bankAccountId,
+        type: filters.type,
+        date: {
+          gte: new Date(Date.UTC(filters.year, filters.month)),
+          lt: new Date(Date.UTC(filters.year, filters.month + 1))
+        }
+      }
+    })
   }
 
   async update(
--- src/modules/transactions/transactions.controller.ts
+++ src/modules/transactions/transactions.controller.ts
@@ -9,12 +9,15 @@ import {
   ParseUUIDPipe,
   HttpCode,
   HttpStatus,
-  Query
+  Query,
+  ParseIntPipe,
+  ParseEnumPipe
 } from '@nestjs/common'
 import { TransactionsService } from './services/transactions.service'
 import { CreateTransactionDto } from './dto/create-transaction.dto'
 import { UpdateTransactionDto } from './dto/update-transaction.dto'
 import { ActiveUserId } from 'src/shared/decorators/ActiveUserId'
+import { TransactionType } from './entities/Transaction'
 
 @Controller('transactions')
 export class TransactionsController {
@@ -31,10 +34,19 @@ export class TransactionsController {
   @Get()
   findAll(
     @ActiveUserId() userId: string,
-    @Query('month') month: number,
-    @Query('year') year: number
+    @Query('month', ParseIntPipe) month: number,
+    @Query('year', ParseIntPipe) year: number,
+    @Query('bankAccountId', new ParseUUIDPipe({ optional: true }))
+    bankAccountId?: string,
+    @Query('type', new ParseEnumPipe(TransactionType, { optional: true }))
+    type?: TransactionType
   ) {
-    return this.transactionsService.findAllByUserId(userId)
+    return this.transactionsService.findAllByUserId(userId, {
+      month,
+      year,
+      bankAccountId,
+      type
+    })
   }
 
   @Put(':id')
